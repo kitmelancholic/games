571d4f5ee19b6912cc4a0fe8212b34325ccea049
Костик Олексій <oleksij.kostyk@gmail.com>
2024-04-19 19:59:38 +0300

    made separate file for all constant values in game

---
+++ Tetris/src/GameConsts.h
@@ -0,0 +1,13 @@
+#ifndef TETRIS_GAMECONSTS_H
+#define TETRIS_GAMECONSTS_H
+
+#include "raylib.h"
+
+const Vector2 kWindowSize = {338, 700};
+const Vector2 kGridOffset = {15, 30};
+const Vector2 kGridSize = {10, 21};
+const Vector2 kGridBoxSize = {30, 30};
+const Vector2 kGridBoxOffset = {1, 1};
+const Color kColors[7] = {LIGHTGRAY, LIME, MAROON, ORANGE, GOLD, VIOLET, DARKBLUE};
+
+#endif //TETRIS_GAMECONSTS_H
